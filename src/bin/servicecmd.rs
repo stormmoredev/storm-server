8e7e57a3baa5b70e1478224885bec7987d990a1a
Michał Czerski <czerski.michal@gmail.com>
2025-09-16 21:12:29 +0200

    Update servicecmd.rs

--- src/bin/servicecmd.rs
+++ src/bin/servicecmd.rs
@@ -1,12 +1,20 @@
 use std::path::PathBuf;
+use tokio::task::JoinHandle;
 use std::time::Duration;
 use storm_server::service::run_storm_service;
 
 #[tokio::main]
 async fn main() {
     tokio::spawn( async move {
-        if let Err(e) = run_storm_service(PathBuf::from("c:\\stormsrv")).await {
-            eprintln!("Error: {}", e);
+        let mut handlers;
+        match run_storm_service(PathBuf::from("c:\\stormsrv")).await {
+            Ok(h) => handlers = h,
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+        loop {
+            tokio::time::sleep(Duration::from_secs(1)).await;
         }
     });
     loop {
